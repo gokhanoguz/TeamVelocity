f8b8c74f2c72ef2d11bda53624f829c4f68b0379
Gokhan Oguz <gokhan.oguz@kindredgroup.com>
2019-11-04 13:03:15 +0100

    Add queries to the dao

--- app/src/main/java/com/pgaa/teamvelocity/data/dao/StaffDao.kt
+++ app/src/main/java/com/pgaa/teamvelocity/data/dao/StaffDao.kt
@@ -1,8 +1,10 @@
 package com.pgaa.teamvelocity.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.pgaa.teamvelocity.data.entity.Staff
 
 @Dao
@@ -10,5 +12,14 @@ interface StaffDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(staff: Staff)
+
+    @Delete
+    suspend fun deleteStaff(vararg staff: Staff)
+
+    @Query("SELECT * from staff_table ORDER BY name ASC")
+    suspend fun getAllStaff(): List<Staff>
+
+    @Query("SELECT * from staff_table WHERE name LIKE :name")
+    suspend fun getStaffByName(name: String)
 
 }
