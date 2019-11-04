45c10086591f344f319e3f52e5352d3a84b65edb
Gokhan Oguz <gokhan.oguz@kindredgroup.com>
2019-11-04 13:53:22 +0100

    Create sprint entity

---
+++ app/src/main/java/com/pgaa/teamvelocity/data/entity/Sprint.kt
@@ -0,0 +1,14 @@
+package com.pgaa.teamvelocity.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sprint_table")
+data class Sprint(
+    @PrimaryKey @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "events") val events: String,
+    @ColumnInfo(name = "staff_day_map") val staffDayMap: Map<Staff, Int>,
+    @ColumnInfo(name = "story_points") val storyPoints: Int,
+    @ColumnInfo(name = "ratio") val ratio: Double
+)
