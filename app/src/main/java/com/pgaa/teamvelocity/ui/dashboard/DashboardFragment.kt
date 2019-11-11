108af7aed0fe84c077a4083d0e25e35af6b0ad28
Gokhan Oguz <gokhan.oguz@kindredgroup.com>
2019-11-11 11:14:47 +0100

    Create a reporting util

--- app/src/main/java/com/pgaa/teamvelocity/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/pgaa/teamvelocity/ui/dashboard/DashboardFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.pgaa.teamvelocity.R
+import com.pgaa.teamvelocity.data.entity.Sprint
 import com.pgaa.teamvelocity.data.model.SprintStats
+import com.pgaa.teamvelocity.util.ReportingUtils
 import com.pgaa.teamvelocity.util.SprintUtils.calculateSprintStats
 
 class DashboardFragment : Fragment() {
@@ -18,8 +21,10 @@ class DashboardFragment : Fragment() {
     private lateinit var avRatioTextView: TextView
     private lateinit var avStoryPointsTextView: TextView
     private lateinit var avManDayTextView: TextView
+    private lateinit var createReportButton: Button
 
     var sprinStats: SprintStats? = null
+    var sprintList: List<Sprint>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,7 +37,9 @@ class DashboardFragment : Fragment() {
         avRatioTextView = root.findViewById(R.id.text_av_ratio_value)
         avStoryPointsTextView = root.findViewById(R.id.text_av_storypoint_value)
         avManDayTextView = root.findViewById(R.id.text_av_manday_value)
+        createReportButton = root.findViewById(R.id.button_create_report)
         dashboardViewModel.allSprints.observe(this, Observer {
+            sprintList = it
             sprinStats = calculateSprintStats(it)
             updateUI()
         })
@@ -46,6 +53,10 @@ class DashboardFragment : Fragment() {
             avRatioTextView.text = "%.2f".format(it.avRatio)
             avStoryPointsTextView.text = "%.2f".format(it.avStoryPoints)
             avManDayTextView.text = "%.2f".format(it.avManDay)
+        }
+
+        if (activity != null && sprintList!= null && sprinStats!= null) {
+            createReportButton.setOnClickListener { ReportingUtils.createFullReportPdf(activity!!, sprintList!!, sprinStats!!) }
         }
     }
 }
---
+++ app/src/main/java/com/pgaa/teamvelocity/util/ReportingUtils.kt
@@ -0,0 +1,67 @@
+package com.pgaa.teamvelocity.util
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import com.pgaa.teamvelocity.data.entity.Sprint
+import com.pgaa.teamvelocity.data.model.SprintStats
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+object ReportingUtils {
+
+    const val TAG = "ReportingUtils"
+
+    const val PAGE_WIDTH = 300
+    const val PAGE_HEIGHT = 600
+    const val PAGE_NUMBER = 1
+
+    fun createFullReportPdf(context: Context, sprintList : List<Sprint>) {
+        createFullReportPdf(context, sprintList, SprintUtils.calculateSprintStats(sprintList))
+    }
+
+    fun createFullReportPdf(context: Context, sprintList: List<Sprint>, sprintStats: SprintStats) {
+        var document = PdfDocument()
+        var pageInfo = PdfDocument.PageInfo.Builder(PAGE_WIDTH, PAGE_HEIGHT, PAGE_NUMBER).create()
+
+        var page = document.startPage(pageInfo)
+        var canvas = page.canvas
+        var paint = Paint()
+
+        paint.color = Color.RED
+        canvas.drawText("Full Sprint Reports", 10.0f, 20.0f, paint)
+
+        document.finishPage(page)
+        writeDocumentToPdfFile(context, document, "FullSprintReport.pdf")
+        document.close()
+
+    }
+
+    private fun writeDocumentToPdfFile(context: Context, document: PdfDocument, fileName: String) {
+
+        if(!hasExternalStoragePrivateDocuments(context)) {
+            Log.e(TAG, "Error: Directory does not exist!")
+            Toast.makeText(context, "Directory does not exist!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val targetFile = File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), fileName)
+        try {
+            document.writeTo(FileOutputStream(targetFile))
+            Toast.makeText(context, "Report is created", Toast.LENGTH_LONG).show()
+        } catch (e: IOException) {
+            Log.e(TAG, "Error: $e")
+            Toast.makeText(context, "Something went wrong!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun hasExternalStoragePrivateDocuments(context: Context): Boolean {
+        val path = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        return path != null
+    }
+}
