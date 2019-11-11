5993dd18a56a20a27aa239ffb79620b02c3193f0
Gokhan Oguz <gokhan.oguz@kindredgroup.com>
2019-11-11 14:42:28 +0100

    Add calculate fragment logic and ui

--- app/src/main/java/com/pgaa/teamvelocity/ui/calculation/CalculationFragment.kt
+++ app/src/main/java/com/pgaa/teamvelocity/ui/calculation/CalculationFragment.kt
@@ -4,28 +4,57 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.pgaa.teamvelocity.R
+import com.pgaa.teamvelocity.data.entity.Sprint
 
 class CalculationFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: CalculationViewModel
+    private lateinit var calculationViewModel: CalculationViewModel
+    private var sprintList : List<Sprint>? = null
+
+    private lateinit var calculateButton: Button
+    private lateinit var resultTextView: TextView
+    private lateinit var expectedManDayEditText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
+        calculationViewModel =
             ViewModelProviders.of(this).get(CalculationViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_calculation, container, false)
-        val textView: TextView = root.findViewById(R.id.text_calculation)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
+        expectedManDayEditText = root.findViewById(R.id.editText_expected_manday)
+        calculateButton = root.findViewById(R.id.button_calculate)
+        calculateButton.setOnClickListener {
+            if (expectedManDayEditText.text.isNullOrEmpty()) {
+                Toast.makeText(activity, "Enter Expected Man Day", Toast.LENGTH_SHORT).show()
+            } else if (sprintList == null) {
+                Toast.makeText(activity, "No Sprint is added", Toast.LENGTH_SHORT).show()
+            } else {
+                calculationViewModel.calculateExpectedStoryPoint(expectedManDayEditText.text.toString(), sprintList!!)
+            }
+
+        }
+        resultTextView = root.findViewById(R.id.textView_result)
+
+        calculationViewModel.allSprints.observe(this, Observer {
+            sprintList = it
         })
+        calculationViewModel.expectedStoryPoint.observe(this, Observer {
+            resultTextView.text = it
+            resultTextView.visibility = View.VISIBLE
+        })
+
+        calculationViewModel.getAllSprint()
+
         return root
     }
 }
--- app/src/main/java/com/pgaa/teamvelocity/ui/calculation/CalculationViewModel.kt
+++ app/src/main/java/com/pgaa/teamvelocity/ui/calculation/CalculationViewModel.kt
@@ -1,13 +1,40 @@
 package com.pgaa.teamvelocity.ui.calculation
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pgaa.teamvelocity.data.entity.Sprint
+import com.pgaa.teamvelocity.data.local.SprintDatabase
+import com.pgaa.teamvelocity.data.repository.SprintRepository
+import com.pgaa.teamvelocity.util.SprintUtils
+import kotlinx.coroutines.launch
 
-class CalculationViewModel : ViewModel() {
+class CalculationViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: SprintRepository
+    var allSprints: MutableLiveData<List<Sprint>>
+    var expectedStoryPoint: MutableLiveData<String>
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is calculation Fragment"
+    init {
+        val sprinData = SprintDatabase.getDatabase(application).sprintDao()
+        repository = SprintRepository(sprinData)
+        allSprints = MutableLiveData()
+        expectedStoryPoint = MutableLiveData()
     }
-    val text: LiveData<String> = _text
+
+    fun getAllSprint() {
+        viewModelScope.launch {
+            allSprints.value = repository.getAllSprints()
+        }
+    }
+
+    fun calculateExpectedStoryPoint(expectedManDay: String, sprintList: List<Sprint>) {
+        viewModelScope.launch {
+            var ratio = SprintUtils.calculateSprintStats(sprintList).avRatio
+
+            expectedStoryPoint.value = "%.2f".format(ratio.toDouble() * expectedManDay.toDouble())
+        }
+    }
 }
