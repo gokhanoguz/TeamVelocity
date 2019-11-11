463fd8004c91857d986eab22cda233bc2813518a
Gokhan Oguz <gokhan.oguz@kindredgroup.com>
2019-11-11 11:29:35 +0100

    Add date to document

--- app/src/main/java/com/pgaa/teamvelocity/util/ReportingUtils.kt
+++ app/src/main/java/com/pgaa/teamvelocity/util/ReportingUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.pdf.PdfDocument
+import android.os.Build
 import android.os.Environment
 import android.util.Log
 import android.widget.Toast
@@ -12,6 +13,11 @@ import com.pgaa.teamvelocity.data.model.SprintStats
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Date
 
 object ReportingUtils {
 
@@ -37,7 +43,7 @@ object ReportingUtils {
         canvas.drawText("Full Sprint Reports", 10.0f, 20.0f, paint)
 
         document.finishPage(page)
-        writeDocumentToPdfFile(context, document, "FullSprintReport.pdf")
+        writeDocumentToPdfFile(context, document, "FullSprintReport_${todaysDate()}.pdf")
         document.close()
 
     }
@@ -63,5 +69,13 @@ object ReportingUtils {
     private fun hasExternalStoragePrivateDocuments(context: Context): Boolean {
         val path = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
         return path != null
+    }
+
+    private fun todaysDate(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE)
+        } else {
+            Date().toString()
+        }
     }
 }
